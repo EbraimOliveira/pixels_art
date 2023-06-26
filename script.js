29688b598d0f8de30b30468bcc263561120c3009
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2023-06-26 10:25:09 -0300

    add hover func

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ const hoverDescription = inputWrapper.querySelector('.description');
 const refreshColorsBtn = document.getElementById('refresh-colors');
 
 const createPallete = () => {
-  Array.from(randomColorPalette).forEach((color) => {
+  Array.from(randomColorPalette).forEach((item) => {
     const newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const color = item;
     color.style.backgroundColor = newColor;
   });
 };
@@ -20,12 +21,11 @@ createPallete();
 const selectBlackColor = () => {
   blackColorPalette.style.backgroundColor = 'black';
   blackColorPalette.classList.add('selected');
-  blackColorPalette.classList.add('border')
+  blackColorPalette.classList.add('border');
 };
 selectBlackColor();
 
 const createPixels = (n) => {
-
   for (let i = 0; i < n * n; i += 1) {
     const pixels = document.createElement('div');
     pixels.classList.add('pixel');
@@ -40,7 +40,7 @@ const removeSelected = (color) => {
   Array.from(colorPalette).forEach((color2) => {
     if (color2 !== color && color2.classList.contains('selected')) {
       color2.classList.remove('selected');
-      color2.classList.remove('border')
+      color2.classList.remove('border');
     }
   });
 };
@@ -48,7 +48,7 @@ const removeSelected = (color) => {
 const addSelected = (color) => {
   if (!color.classList.contains('selected')) {
     color.classList.add('selected');
-    color.classList.add('border')
+    color.classList.add('border');
     removeSelected(color);
   }
 };
@@ -62,8 +62,30 @@ const selectColor = () => {
 };
 selectColor();
 
+const hover = () => {
+  let isMouseDown = false;
+
+  document.addEventListener('mousedown', () => {
+    isMouseDown = true;
+  });
+
+  document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+  });
+
+  pixelBoard.addEventListener('mouseover', (e) => {
+    if (isMouseDown) {
+      const selectedColor = document.getElementsByClassName('selected')[0];
+      if (e.target !== document.getElementById('pixel-board')) {
+        e.target.style.backgroundColor = selectedColor.style.backgroundColor;
+      }
+    }
+  });
+};
+
 const applyColor = () => {
   pixelBoard.addEventListener('click', (e) => {
+    hover();
     const selectedColor = document.getElementsByClassName('selected')[0];
     if (e.target !== document.getElementById('pixel-board')) {
       e.target.style.backgroundColor = selectedColor.style.backgroundColor;
@@ -75,7 +97,8 @@ applyColor();
 const clearBoard = () => {
   clearButton.addEventListener('click', () => {
     const pixels = document.querySelectorAll('.pixel');
-    Array.from(pixels).forEach((pixel) => {
+    Array.from(pixels).forEach((item) => {
+      const pixel = item;
       pixel.style.backgroundColor = '#fff';
     });
   });
@@ -105,15 +128,15 @@ const newPixelBoard = () => {
 
     if (newBoard === '' || newBoard < 1) {
       alert('Board inválido!');
-      boardSize.value = ''
+      boardSize.value = '';
     }
     if (newBoard > 40) {
       newBoard = 40;
-      boardSize.value = 40
+      boardSize.value = 40;
     }
 
     createPixels(newBoard);
-    clearPixel()
+    clearPixel();
   });
 };
 newPixelBoard();
